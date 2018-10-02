531d38f2bbfed2f9b29b0aaca3b24c8aae457b11
Ron Mason <WHERESRONALD0@GMAIL.COM>
2018-10-02 15:44:54 -0400

    added the user login func and verified salt and hash in mongo- user register send to secret page

--- app.js
+++ app.js
@@ -23,18 +23,46 @@ app.use(
 app.set("view engine", "ejs");
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+//ROUTES
 app.get("/", (req, res) => {
   res.render("Home");
 });
 
-app.listen(4000, () => {
-  console.log("Auth server is up!");
-});
-
 app.get("/secret", (req, res) => {
   res.render("secret");
+});
+
+//Auth Routes
+//show sign up for
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+//handling user sign up info
+app.post("/register", (req, res) => {
+  req.body.username;
+  req.body.password;
+  User.register(
+    new User({ username: req.body.username }),
+    req.body.password,
+    (err, user) => {
+      if (err) {
+        console.log("error");
+        return res.render("register");
+      } else {
+        passport.authenticate("local")(req, res, () => {
+          res.redirect("/secret");
+        });
+      }
+    }
+  );
+});
+
+app.listen(4000, () => {
+  console.log("Auth server is up!");
 });
